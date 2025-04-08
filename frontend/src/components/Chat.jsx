bdf859f3de9600986813dc704fd75a94ec61d63b
Ignacio Gomez <Nachorojo2015@gmail.com>
2025-04-08 14:22:21 -0300

    fix avatar chat image

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -125,14 +125,14 @@ function openMenu() {
         <button onClick={closeChatMobile} className="xl:hidden">
            <FaArrowLeft className="dark:text-white"/>
         </button>
-        <img src={image} alt="picture-chat" className="xl:w-14 xl:h-14 w-10 h-10 rounded-full object-cover" onClick={() => pictureGroupModal.current.showModal()} onError={e => e.target.src = "/picture-no-load.png"}/>
+        <img src={image} alt="picture-chat" className="xl:w-14 xl:h-14 w-10 h-10 rounded-full object-cover" onClick={() => pictureGroupModal.current.showModal()} onError={e => e.target.src = `${type === 'private' ? '/picture-user-no-load.png' : '/picture-group-no-load.png'}`}/>
         <span className="dark:text-white whitespace-nowrap overflow-hidden text-ellipsis">{name}</span>
         <OptionsChat chatId={id} socket={socket} BACKEND_URL={BACKEND_URL}/>
 
         <dialog ref={pictureGroupModal} className="backdrop:bg-[rgba(0,0,0,.90)] xl:max-w-96 max-w-60 outline-none" onClick={() => pictureGroupModal.current.close()}>
           <div>
             <span className="absolute whitespace-nowrap overflow-hidden text-ellipsis bg-black p-2 w-full text-white bg-opacity-40">{name}</span>
-            <img src={image} alt="picture-group" className="object-cover xl:w-96 xl:h-96 w-64 h-64" onError={e => e.target.src = '/picture-group-no-load.png'}/>
+            <img src={image} alt="picture-group" className="object-cover xl:w-96 xl:h-96 w-64 h-64" onError={e => e.target.src = `${type === 'private' ? '/picture-user-no-load.png' : '/picture-group-no-load.png'}`}/>
           </div>
         </dialog>
       </header>
